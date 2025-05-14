92e58b60ef771a436af008ca8d15de85d267bb45
Matt Hiddle <matt.hiddle@gmail.com>
2025-05-14 13:12:13 +0800

    add ability to pass in names

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,10 +2,12 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("world"))
 }
 
-func Hello() string {
-	return "Hello, world"
+func Hello(name string) string {
+	return englishHelloPrefix + name
 }
