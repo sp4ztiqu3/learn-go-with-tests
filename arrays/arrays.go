56cd4192d8900e09002a05653c96704e4a9c1d2b
Matt Hiddle <matt.hiddle@gmail.com>
2025-05-14 21:29:31 +0800

    Change Sum function to work on slices of any size

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -1,9 +1,18 @@
 package arrays
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, num := range numbers {
 		sum += num
 	}
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
 }
--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -1,13 +1,27 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	sum := Sum(numbers)
-	want := 15
+	t.Run("slice of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		sum := Sum(numbers)
+		want := 6
 
-	if sum != want {
-		t.Errorf("got %d, want %d, given %v", sum, want, numbers)
+		if sum != want {
+			t.Errorf("got %d, want %d, given %v", sum, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %d, want %d", got, want)
 	}
 }
