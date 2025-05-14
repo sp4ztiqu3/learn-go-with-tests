bd51dc4a8a79439d73b76a654e10ec56bdcc482e
Matt Hiddle <matt.hiddle@gmail.com>
2025-05-14 13:37:10 +0800

    add ability to specify Spanish greeting

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,15 +2,23 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	spanish            = "Spanish"
+)
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
 
-func Hello(name string) string {
+func Hello(name, lang string) string {
 	if name == "" {
 		name = "World"
+	}
+
+	if lang == spanish {
+		return spanishHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
