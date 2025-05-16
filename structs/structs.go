416800291015d8d4f15311530d531c003e6f17b2
Matt Hiddle <matt.hiddle@gmail.com>
2025-05-16 18:15:17 +0800

    Add Area function

--- structs/structs.go
+++ structs/structs.go
@@ -1,5 +1,32 @@
 package structs
 
-func Perimeter(length, width float64) float64 {
-	return 2 * (length + width)
+import "math"
+
+type Rectangle struct {
+	Length float64
+	Width  float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Length * r.Width
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+type Shape interface {
+	Area() float64
+}
+
+func Perimeter(rect Rectangle) float64 {
+	return 2 * (rect.Length + rect.Width)
+}
+
+func Area(rect Rectangle) float64 {
+	return rect.Length * rect.Width
 }
--- structs/structs_test.go
+++ structs/structs_test.go
@@ -3,10 +3,32 @@ package structs
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rect := Rectangle{Length: 10.0, Width: 10.0}
+	got := Perimeter(rect)
 	want := 40.0
 
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	}
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{10.0, 10.0}, 100.0},
+		{Circle{10}, 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		checkArea(t, tt.shape, tt.want)
 	}
 }
