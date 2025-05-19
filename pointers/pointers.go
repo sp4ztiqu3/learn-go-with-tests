20b27ddb71810d8bd01475b2f3a8fc47ba418b69
Matt Hiddle <matt.hiddle@gmail.com>
2025-05-19 21:27:13 +0800

    Add Bitcoin Wallet to pointers package

---
+++ pointers/pointers.go
@@ -0,0 +1,21 @@
+package pointers
+
+import "fmt"
+
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
