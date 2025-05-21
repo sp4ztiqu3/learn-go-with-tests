1b35e95e008d2b7a76f486ff3abb9f523741ac20
Matt Hiddle <matt.hiddle@gmail.com>
2025-05-21 18:21:19 +0800

    Add countdown

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -3,8 +3,28 @@ package mocking
 import (
 	"fmt"
 	"io"
+	"time"
 )
 
-func Countdown(out io.Writer) {
-	fmt.Fprintf(out, "3")
+const (
+	countdownStart = 3
+	finalWord      = "Go!"
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+	fmt.Fprint(out, finalWord)
 }
--- mocking/mocking_test.go
+++ mocking/mocking_test.go
@@ -5,15 +5,31 @@ import (
 	"testing"
 )
 
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
 
-	Countdown(buffer)
+	Countdown(buffer, spySleeper)
 
 	got := buffer.String()
-	want := "3"
+	want := `3
+2
+1
+Go!`
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+
+	if spySleeper.Calls != 3 {
+		t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
 	}
 }
