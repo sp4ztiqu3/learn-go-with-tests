cdee24910f05997c85f5d00c9f6234ef8c9746d3
Matt Hiddle <matt.hiddle@gmail.com>
2025-05-26 17:07:49 +0800

    Add test confirming correct order of Countdown messages

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -15,10 +15,13 @@ type Sleeper interface {
 	Sleep()
 }
 
-type DefaultSleeper struct{}
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
 
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
 }
 
 func Countdown(out io.Writer, sleeper Sleeper) {
