fa713b03f076b331a69e6916b76e4af91c79ed98
Matt Hiddle <matt.hiddle@gmail.com>
2025-05-29 17:12:08 +0800

    Make Counter safe for concurrency

--- sync/sync.go
+++ sync/sync.go
@@ -1,10 +1,20 @@
 package sync
 
+import "sync"
+
 type Counter struct {
 	value int
+	mu    sync.Mutex
+}
+
+func NewCounter() *Counter {
+	return &Counter{}
 }
 
 func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	c.value++
 }
 
