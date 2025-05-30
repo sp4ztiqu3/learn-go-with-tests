d25278a7b6eff5b3179b33f0beaa768b4b90a186
Matt Hiddle <matt.hiddle@gmail.com>
2025-05-30 22:16:12 +0800

    Add support for conversions up to 3999

--- roman/roman_test.go
+++ roman/roman_test.go
@@ -1,38 +1,51 @@
 package roman
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRomanNumerals(t *testing.T) {
 	cases := []struct {
-		Description string
-		Arabic      int
-		Want        string
+		Arabic int
+		Roman  string
 	}{
-		{"1 to I", 1, "I"},
-		{"2 to II", 2, "II"},
-		{"3 to III", 3, "III"},
-		{"4 to IV (max 3 repeats", 4, "IV"},
-		{"5 to V", 5, "V"},
-		{"6 to VI", 6, "VI"},
-		{"7 to VII", 7, "VII"},
-		{"8 to VIII", 8, "VIII"},
-		{"9 to IX", 9, "IX"},
-		{"10 to X", 10, "X"},
-		{"14 to XIV", 14, "XIV"},
-		{"18 to XVIII", 18, "XVIII"},
-		{"20 to XX", 20, "XX"},
-		{"39 to XXXIX", 39, "XXXIX"},
-		{"40 to XL", 40, "XL"},
-		{"47 to XLVII", 47, "XLVII"},
-		{"49 to XLIX", 49, "XLIX"},
-		{"50 to L", 50, "L"},
+		{1, "I"},
+		{2, "II"},
+		{3, "III"},
+		{4, "IV"},
+		{5, "V"},
+		{6, "VI"},
+		{7, "VII"},
+		{8, "VIII"},
+		{9, "IX"},
+		{10, "X"},
+		{14, "XIV"},
+		{18, "XVIII"},
+		{20, "XX"},
+		{39, "XXXIX"},
+		{40, "XL"},
+		{47, "XLVII"},
+		{49, "XLIX"},
+		{50, "L"},
+		{Arabic: 100, Roman: "C"},
+		{Arabic: 90, Roman: "XC"},
+		{Arabic: 400, Roman: "CD"},
+		{Arabic: 500, Roman: "D"},
+		{Arabic: 900, Roman: "CM"},
+		{Arabic: 1000, Roman: "M"},
+		{Arabic: 1984, Roman: "MCMLXXXIV"},
+		{Arabic: 3999, Roman: "MMMCMXCIX"},
+		{Arabic: 2014, Roman: "MMXIV"},
+		{Arabic: 1006, Roman: "MVI"},
+		{Arabic: 798, Roman: "DCCXCVIII"},
 	}
 
 	for _, test := range cases {
-		t.Run(test.Description, func(t *testing.T) {
+		t.Run(fmt.Sprintf("%d to %q", test.Arabic, test.Roman), func(t *testing.T) {
 			got := ConvertToRoman(test.Arabic)
-			if got != test.Want {
-				t.Errorf("got %q, want %q", got, test.Want)
+			if got != test.Roman {
+				t.Errorf("got %q, want %q", got, test.Roman)
 			}
 		})
 	}
