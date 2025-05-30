776ce170d015a2bf10885c2f094c443c8feb349c
Matt Hiddle <matt.hiddle@gmail.com>
2025-05-30 21:56:43 +0800

    Add tests for 6,7,8

--- roman/roman_test.go
+++ roman/roman_test.go
@@ -13,6 +13,9 @@ func TestRomanNumerals(t *testing.T) {
 		{"3 to III", 3, "III"},
 		{"4 to IV (max 3 repeats", 4, "IV"},
 		{"5 to V", 5, "V"},
+		{"6 to VI", 6, "VI"},
+		{"7 to VII", 7, "VII"},
+		{"8 to VIII", 8, "VIII"},
 	}
 
 	for _, test := range cases {
