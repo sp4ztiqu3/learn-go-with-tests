33019315cbddca920383389596da7dc443c2d63b
Matt Hiddle <matt.hiddle@gmail.com>
2025-05-30 21:41:35 +0800

    Add ability to convert 2 to II

--- roman/roman_test.go
+++ roman/roman_test.go
@@ -11,4 +11,30 @@ func TestRomanNumerals(t *testing.T) {
 			t.Errorf("got %q, want %q", got, want)
 		}
 	})
+	t.Run("2 to II", func(t *testing.T) {
+		got := ConvertToRoman(2)
+		want := "II"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	cases := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{"1 to I", 1, "I"},
+		{"2 to II", 2, "II"},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+			if got != test.Want {
+				t.Errorf("got %q, want %q", got, test.Want)
+			}
+		})
+	}
 }
